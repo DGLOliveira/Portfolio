af1fc639c9bd438bc24f8f7379e6e6ceb3171b15
DGLOliveira <dgloliveira90@gmail.com>
2026-01-03 16:12:00 -0100

    Added animations to intro text

--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+
 export default function Intro() {
+    const [introIndex, setIntroIndex] = useState(-1)
+    const [introText, setIntroText] = useState("")
+    const [welcomeOpacity, setWelcomeOpacity] = useState(0)
+    const INTRO = "My name is Davide, I'm an aspiring, self-taught web developer."
+
+    useEffect(() => {
+        console.log(welcomeOpacity)
+        if (introIndex === -1) {
+            if (welcomeOpacity < 1) {
+                setTimeout(() => {
+                    setWelcomeOpacity(welcomeOpacity => welcomeOpacity + 0.05)
+                }, 30)
+            } else {
+                setTimeout(() => {
+                    setIntroIndex(0)
+                }, 1000)
+            }
+        }
+    }, [welcomeOpacity])
+
+    useEffect(() => {
+        console.log(introIndex)
+        if (introIndex > -1 && introIndex < INTRO.length) {
+            const interval = setInterval(() => {
+                setIntroText(INTRO.substring(0, introIndex + 1))
+                setIntroIndex(introIndex => introIndex + 1)
+            }, 30);
+            return () => clearInterval(interval)
+        }
+    }, [introIndex])
+
     return (
         <div id="intro">
             <div>
-                <h1>Welcome</h1>
+                <h1 style={{ opacity: welcomeOpacity }}>Welcome</h1>
                 <span />
-                <p>My name is <b>Davide</b>, I'm an aspiring, self-taught <b>web developer</b>.</p>
+                <p>{introText}</p>
             </div>
         </div >
     );
