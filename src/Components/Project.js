3ad50856bd9bb8275734ade67bb718ec7fa0aa0d
DGLOliveira <dgloliveira90@gmail.com>
2026-01-04 16:00:18 -0100

    Modified projects_info and Project.js in order to contain multiple images

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { FaGithub, FaLink, FaFilePdf } from "react-icons/fa";
-export default function Projects({project, index}) {
+export default function Projects({ project, index }) {
+    const [slide, setSlide] = useState(0);
 
     return (
         <div key={index} id={"project" + index} className="project">
             <div>
                 <h2 className="projectName">{project.name}</h2>
                 <div>
-                    <div className="projectImage">
-                        <img src={project.image} />
+                    <div className="projectSlides">
+                        <div className="projectSlideContainer">
+                            {project.slides.map((slide, index) => {
+                                return (
+                                    <img
+                                        key={index}
+                                        onClick={() => {
+                                            setSlide(index);
+                                        }}
+                                        src={slide.url}
+                                        className={slide.type === "image" ? "projectSlideImage" : "projectSlideVideo"}
+                                        alt={slide.title}
+                                    />
+                                )
+                            })}
+                        </div>
                     </div>
                     <div>
                         <div className="projectDescription">
